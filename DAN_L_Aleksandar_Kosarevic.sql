9581479e61804320fc29afbf50b72350c8d62815
Kosarevic <kosarevic@gmail.com>
2020-08-05 11:35:00 +0200

    Added model classes and implemented SQL database.

---
+++ DAN_L_Aleksandar_Kosarevic.sql
@@ -0,0 +1,27 @@
+--If database doesnt exist it is automatically created.
+IF DB_ID('Zadatak_1') IS NULL
+CREATE DATABASE Zadatak_1
+GO
+--Newly created database is set to be in use.
+USE Zadatak_1
+--All tables are reseted clean.
+if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblSong')
+drop table tblSong
+if exists (SELECT name FROM sys.sysobjects WHERE name = 'tblUser')
+drop table tblUser
+
+create table tblUser
+(
+UserID int primary key IDENTITY(1,1),
+Username varchar(50),
+Password varchar(50)
+)
+
+create table tblSong
+(
+SongID int primary key IDENTITY(1,1),
+Title varchar(50),
+Author varchar(50),
+Length time,
+UserID int foreign key references tblUser(UserID) not null
+)
---
+++ Zadatak_1/Model/Song.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1.Model
+{
+    class Song
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public TimeSpan Length { get; set; }
+
+        public Song()
+        {
+        }
+
+        public Song(int id, string title, string author, TimeSpan length)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            Length = length;
+        }
+    }
+}
